56cf82f6934015daf5137ca7f12e2fafdab612e0
Avni Rexhepi <arexhepi@gmail.com>
2015-05-05 01:00:17 -0600

    Second draft

--- src/Avnir/Easyrouting/EasyRouting.php
+++ src/Avnir/Easyrouting/EasyRouting.php
@@ -1,22 +1,25 @@
 <?php namespace Avnir\Easyrouting;
 
-function EasyRouting()
-{
-    foreach(glob(__DIR__.'/controllers/*.php') as $filename) {
-        $file_parts = explode('/', $filename);
-        $file = array_pop($file_parts);
-        $file = rtrim($file,'.php');
-        $controller = new $file();
-        if($controller->exclude === true)
-            continue;
+class EasyRouting {
 
-        $reflector = new ReflectionClass($controller);
-        $methods = [];
-        foreach ($reflector->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-            if ($method->class == $reflector->getName())
-                $methods[] = $method->name;
+    function EasyRouting()
+    {
+        foreach(glob(__DIR__.'/controllers/*.php') as $filename) {
+            $file_parts = explode('/', $filename);
+            $file = array_pop($file_parts);
+            $file = rtrim($file,'.php');
+            $controller = new $file();
+            if($controller->exclude === true)
+                continue;
+
+            $reflector = new ReflectionClass($controller);
+            $methods = [];
+            foreach ($reflector->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+                if ($method->class == $reflector->getName())
+                    $methods[] = $method->name;
+            }
+
+            Route::resource($controller->name,                                    $file,['only'=>$methods]);
         }
-
-        Route::resource($controller->name,                                    $file,['only'=>$methods]);
     }
 }
